17b96c18f4b659351bcf16e618dafa5a0a5bba1e
Keydra Burtin <bkeydra@gmail.com>
2023-04-14 17:16:49 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() *10) +1;
+
+let num3 = 12;
+let num4 = 4;
+
+let mod = num4 % num3;
+
+let num5 = 5;
+let num6 = 20;
+
+let max = Math.max(num5,num6);
